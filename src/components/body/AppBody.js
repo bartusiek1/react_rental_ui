d81d589e0942b2b7e36a112cb6d140cb468ef6de
Your Name <grubabartek@gmail.com>
2022-07-20 20:57:07 +0200

    konfiguracja axiosa

--- src/components/body/AppBody.js
+++ src/components/body/AppBody.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from './AppBody.module.css'
 import {Grid} from "@mui/material";
+import connection from "../../axios/axios";
 
 const AppBody = () => {
+    const [carList, setCarList] = useState([]);
+
+    useEffect(() => {
+        connection.get("/api/car/list")       // fire and forget
+            .then((response) => {
+                console.log("OK! ");
+                console.log(response);
+                setCarList(response.data);
+            })
+            .catch((errorResponse) => {
+                console.log("ErRoR: " + errorResponse);
+            });
+    }, []);
+
+    console.log("Przeładowuje")
+
     return (
         <div className={classes.AppBody}>
             <div className={classes.BodyContainer}>
@@ -19,6 +36,20 @@ const AppBody = () => {
                             <Grid item xs={2}>Gearbox</Grid>
                             <Grid item xs={2}></Grid>
                         </Grid>
+                    {
+                        carList.map((elementListy) =>{
+                            return (
+                                <Grid key={elementListy.carId} container className={classes.TableHeader}>{/*wiersz*/}
+                                <Grid item xs={2}>{elementListy.carId}</Grid>
+                                <Grid item xs={2}>{elementListy.name}</Grid>
+                                <Grid item xs={2}>{elementListy.make}</Grid>
+                                <Grid item xs={2}>{elementListy.bodyType}</Grid>
+                                <Grid item xs={2}>{elementListy.carGearBox}</Grid>
+                                <Grid item xs={2}></Grid>
+                            </Grid>
+                            )
+                        })
+                    }
                     </Grid>
                 </div>
             </div>
